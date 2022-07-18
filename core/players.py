f4b6e1b7b2e3e588f6daccec6b27646b18d9dfac
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-07-18 02:56:32 -0400

    feat: implement func random_players

--- core/players.py
+++ core/players.py
@@ -1,3 +1,5 @@
+import random
+
 from core.behaviors import Behaviors
 
 
@@ -8,8 +10,22 @@ class Players:
         index = 0
         for param in Behaviors.__dict__.values():
             if hasattr(param, "__call__"):
-                behavior = param.__name__
-                index += 1
-                players[index] = {"behavior": behavior, "balance": 300}
+                if not param.__name__ == "__init__":
+                    behavior = param.__name__
+                    index += 1
+                    players[index] = {"behavior": behavior, "balance": 300}
 
         return players
+
+    @staticmethod
+    def randon_players(players: dict) -> dict:
+        player_values = list(players.keys())
+        position = 0
+        random.shuffle(player_values)
+        randomized_players = dict()
+
+        for key in player_values:
+            position += 1
+            randomized_players.update({position: players[key]})
+
+        return randomized_players
