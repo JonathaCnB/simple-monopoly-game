059e4c0413fac055af0e19aec8d4ef9db8a3de3b
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-07-18 02:57:09 -0400

    fix: fix value property

--- core/property.py
+++ core/property.py
@@ -5,5 +5,5 @@ class Property:
     def __init__(self, position: int, owner: str = ""):
         self.position = position
         self.owner = owner
-        self.value = random_between(30, 450)
+        self.value = random_between(30, 150)
         self.rent = round(self.value * 0.4)
