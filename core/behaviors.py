1a10addbe821ce08e3c7ec2d38cf71d97dc1b7e9
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-07-18 02:54:23 -0400

    fix: fix changing class behaviors

--- core/behaviors.py
+++ core/behaviors.py
@@ -2,26 +2,29 @@ import random
 
 
 class Behaviors:
-    @staticmethod
-    def impulsive(property_value: float, balance: float) -> bool:
-        if property_value <= balance:
+    def __init__(
+        self, property_value: float, property_rent: float, balance: float
+    ) -> None:
+        self.property_value = property_value
+        self.property_rent = property_rent
+        self.balance = balance
+
+    def impulsive(self) -> bool:
+        if self.property_value <= self.balance:
             return True
         return False
 
-    @staticmethod
-    def demanding(property_rent: float) -> bool:
-        if property_rent > 50:
+    def demanding(self) -> bool:
+        if self.property_rent > 50:
             return True
         return False
 
-    @staticmethod
-    def cautious(property_value: float, balance: float) -> bool:
-        balance_after_purchase = balance - property_value
+    def cautious(self) -> bool:
+        balance_after_purchase = self.balance - self.property_value
         if balance_after_purchase > 80:
             return True
         return False
 
-    @staticmethod
-    def randomized() -> bool:
+    def randomized(self) -> bool:
         likelihood = bool(random.randint(0, 1))
         return likelihood
