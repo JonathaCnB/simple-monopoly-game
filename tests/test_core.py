2b1525af4566ec93acf8850912394b61705961e2
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-07-18 02:57:50 -0400

    test: refactor

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,32 +4,34 @@ from core.behaviors import Behaviors
 
 
 class TestBehaviors(unittest.TestCase):
+    def setUp(self) -> None:
+        property_value = 80.00
+        property_rent = 32.00
+        balance = 300.00
+        self.behaviors = Behaviors(property_value, property_rent, balance)
+        return super().setUp()
+
     def test_impulsive_behavior_buying(self):
         """
         impulsive behavior buying when the value of the property
         is less than or equal to the balance
         """
-        property_value = 80.00
-        balance = 300.00
-        impulsive = Behaviors.impulsive(property_value, balance)
+        impulsive = self.behaviors.impulsive()
         self.assertTrue(impulsive)
 
     def test_demanding_behavior_buying(self):
         """
         demanding behavior only buys if the rent is more than 50
         """
-        property_rent = 80.00
-        demanding = Behaviors.demanding(property_rent)
-        self.assertTrue(demanding)
+        demanding = self.behaviors.demanding()
+        self.assertFalse(demanding)
 
     def test_cautious_behavior_buying(self):
         """
         cautious behavior buys any property as long as there
         is a balance of 80
         """
-        property_value = 80.00
-        balance = 300.00
-        cautious = Behaviors.cautious(property_value, balance)
+        cautious = self.behaviors.cautious()
         self.assertTrue(cautious)
 
     def test_randomized_behavior_buying(self):
